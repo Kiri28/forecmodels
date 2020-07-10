8292cf4dcfe0f9ef61b68bce060ecf36ba2f3969
Kiri28 <33180775+Kiri28@users.noreply.github.com>
2020-07-10 21:26:05 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as readme_file:
 requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
 
 setup(
-    name="notebookc",
+    name="forecmodels",
     version="0.0.1",
     author="Jeff Hale",
     author_email="jeffmshale@gmail.com",
