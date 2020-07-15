06640bde3b34ff8a12f67af2070a7ac7201213c2
Kiri28 <33180775+Kiri28@users.noreply.github.com>
2020-07-15 05:42:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+#from distutils.core import setup, Extension
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
@@ -7,13 +8,13 @@ requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
 
 setup(
     name="forecmodels",
-    version="0.0.1",
-    author="Jeff Hale",
-    author_email="jeffmshale@gmail.com",
+    version="0.0.2",
+    author="Kirill Mansurov",
+    author_email="mantis1999@mail.ru",
     description="A package to building forecasting models",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="https://github.com/Kiri28/forecmodels/",
+    url="https://github.com/Kiri28/forecmodels",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
